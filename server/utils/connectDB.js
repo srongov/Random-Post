e05350dbe4696fdebb7a519c6cad3d6a13ccf02c
srongov <srongov@yahoo.com>
2022-05-12 19:37:22 -0400

    w/e

--- server/utils/connectDB.js
+++ server/utils/connectDB.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 
+const MONGO_URI =
+  'mongodb+srv://admin:tcoGaWjvZhSBBJRT@main.mbsxe.mongodb.net/random-post?retryWrites=true&w=majority'
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
