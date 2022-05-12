8ad40836d08396044433ca09fb02b9c1c5978087
srongov <srongov@yahoo.com>
2022-05-12 14:43:34 -0400

    sever

--- server/models/Post.js
+++ server/models/Post.js
@@ -4,23 +4,23 @@ const postSchema = new Schema({
   body: String,
   username: String,
   createdAt: String
-  // comments: [
-  //   {
-  //     body: String,
-  //     username: String,
-  //     createdAt: String
-  //   }
-  // ],
-  // likes: [
-  //   {
-  //     username: String,
-  //     createdAt: String
-  //   }
-  // ],
-  // user: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: 'User'
-  // }
+  comments: [
+    {
+      body: String,
+      username: String,
+      createdAt: String
+    }
+  ],
+  likes: [
+    {
+      username: String,
+      createdAt: String
+    }
+  ],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 module.exports = model('Post', postSchema)
