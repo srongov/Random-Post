50dba4d83bbb536fe9ff18e36a30205d0ad3e283
srongov <srongov@yahoo.com>
2022-05-12 17:31:51 -0400

    trigger

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import { client } from "./Apollo";
 import App from "./App";
 
 // css
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
